007def2cb2623f6e34a3c123ad175d05421478c5
Alistair McKinnell <alistair.mckinnell@gmail.com>
2013-09-11 20:58:32 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+The String Calculator Code Kata
+===============================
+
+Roy Osherov's [String Calculator](http://osherove.com/tdd-kata-1/) code kata.
---
+++ src/test/java/com/valuablecode/kata/StringCalculatorTest.java
@@ -0,0 +1,13 @@
+package com.valuablecode.kata;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class StringCalculatorTest {
+
+    @Test
+    public void describe_behaviour() {
+        fail();
+    }
+}
