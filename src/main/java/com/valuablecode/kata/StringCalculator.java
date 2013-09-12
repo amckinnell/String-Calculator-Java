03c3866295731bc83368f070329fd1e5c9419c6a
Alistair McKinnell <alistair.mckinnell@gmail.com>
2013-09-11 21:45:18 -0400

    Initial solution commit.
    Implement the remaining requirements: handle newline delimiter, ignore numbers
    greater than one thousand, support custom delimiters, and disallow negatives.

--- src/main/java/com/valuablecode/kata/StringCalculator.java
+++ src/main/java/com/valuablecode/kata/StringCalculator.java
@@ -1,21 +1,95 @@
 package com.valuablecode.kata;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringCalculator {
 
     public int add(String numbers) {
-        int sum = 0;
+        List<Integer> values = convertToInteger(parseValues(numbers));
 
-        for (String value : parse(numbers)) {
-            sum += Integer.parseInt(value);
-        }
+        failWhenContainsNegatives(values);
 
-        return sum;
+        return sumOf(values);
     }
 
-    private String[] parse(String numbers) {
-        if (numbers.isEmpty()) return new String[] {};
+    private String[] parseValues(String numbers) {
+        if (numbers.isEmpty()) {
+            return new String[] {};
+        }
 
-        return numbers.split(",");
+        if (containsCustomDelimiters(numbers)) {
+            return parseCustomDelimitedValues(numbers);
+        }
+
+        return parseStandardDelimiters(numbers);
+    }
+
+    private boolean containsCustomDelimiters(String numbers) {
+        return numbers.startsWith("//");
+    }
+
+    private String[] parseCustomDelimitedValues(String numbers) {
+        int newlineIndex = numbers.indexOf('\n');
+
+        String rawCustomDelimiters = numbers.substring(2, newlineIndex);
+        String numberList = numbers.substring(newlineIndex + 1);
+
+        for (String customDelimiter : parseCustomDelimiters(rawCustomDelimiters)) {
+            numberList = numberList.replaceAll(quoteRegularExpression(customDelimiter), ",");
+        }
+
+        return numberList.split(",");
+    }
+
+    private String[] parseCustomDelimiters(String rawCustomDelimiters) {
+        return rawCustomDelimiters.replaceAll("\\[", "").split("\\]");
+    }
+
+    private String quoteRegularExpression(String customSeparator) {
+        return "\\Q" + customSeparator + "\\E";
+    }
+
+    private String[] parseStandardDelimiters(String numbers) {
+        return numbers.split("[,\n]");
+    }
+
+    private List<Integer> convertToInteger(String[] numbers) {
+        List<Integer> result = new ArrayList<Integer>();
+
+        for (String number : numbers) {
+            result.add(toInteger(number));
+        }
+
+        return result;
+    }
+
+    private Integer toInteger(String number) {
+        Integer value = Integer.parseInt(number);
+
+        return value <= 1000 ? value : 0;
+    }
+
+    private void failWhenContainsNegatives(List<Integer> values) {
+        List<Integer> negatives = new ArrayList<Integer>();
+
+        for (Integer value : values) {
+            if (value < 0) negatives.add(value);
+        }
+
+        if (!negatives.isEmpty()) {
+            throw new RuntimeException("Error: negatives not allowed " + negatives);
+        }
+    }
+
+    private int sumOf(List<Integer> values) {
+        int result = 0;
+
+        for (Integer value : values) {
+            result += value;
+        }
+
+        return result;
     }
 
 }
--- src/test/java/com/valuablecode/kata/StringCalculatorTest.java
+++ src/test/java/com/valuablecode/kata/StringCalculatorTest.java
@@ -1,7 +1,10 @@
 package com.valuablecode.kata;
 
+import static org.hamcrest.CoreMatchers.both;
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
@@ -17,12 +20,45 @@ public class StringCalculatorTest {
     @Test
     public void adds_numbers_list_to_sum_of_numbers() {
         assertThat(sut.add("1"), equalTo(1));
-        assertThat(sut.add("2"), equalTo(2));
-
         assertThat(sut.add("1,2"), equalTo(3));
         assertThat(sut.add("2,3"), equalTo(5));
-
         assertThat(sut.add("1,2,3,4"), equalTo(10));
+    }
+
+    @Test
+    public void add_numbers_List_to_sum_of_numbers_when_newline_delimiter() {
+        assertThat(sut.add("1,2\n3"), equalTo(6));
+    }
+
+    @Test
+    public void add_ignores_large_numbers() {
+        assertThat(sut.add("999,1000,1001"), equalTo(1999));
+    }
+
+    @Test
+    public void add_numbers_List_to_sum_of_numbers_when_simple_custom_delimiter() {
+        assertThat(sut.add("//;\n1;2"), equalTo(3));
+        assertThat(sut.add("//x\n1x2x3"), equalTo(6));
+        assertThat(sut.add("//***\n1***2***3***4"), equalTo(10));
+    }
+
+    @Test
+    public void add_numbers_List_to_sum_of_numbers_when_multiple_custom_delimiters() {
+        assertThat(sut.add("//[***]\n1***2"), equalTo(3));
+        assertThat(sut.add("//[;][xx]\n1;2xx3"), equalTo(6));
+        assertThat(sut.add("//[x][y]\n1x2y3x4"), equalTo(10));
+    }
+
+    @Test
+    public void add_fails_for_negative_numbers() {
+        try {
+            sut.add("1,-2,3,-4");
+            fail();
+        } catch (RuntimeException expected) {
+            assertThat(expected.getMessage(),
+                    both(containsString("negatives not allowed")).and(containsString("[-2, -4]")));
+        }
+
     }
 
 }
